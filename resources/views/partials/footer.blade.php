9f58b7428e1c9407b447a7940452366623d1b4f5
musmula <oliver.dvorski@gmail.com>
2017-03-25 12:59:08 +0100

    Update the footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,5 +3,29 @@
         <p>Released under the <a href="https://opensource.org/licenses/mit-license.php">MIT</a> license</p>
         <p>A thing by <a href="https://dvorski.tech">Oliver Dvorski</a></p>
         <p>In association with <a href="https://mittagsfreunde.de">Mittagsfreunde</a></p>
+        <p>
+            <a href="https://github.com/Musmula" target="_blank" class="icon">
+                <i class="fa fa-github"></i>
+            </a>
+
+            <a href="https://twitter.com/Musmula3" target="_blank" class="icon">
+                <i class="fa fa-twitter"></i>
+            </a>
+
+            <a href="https://plus.google.com/+OliverDvorski" target="_blank" class="icon">
+                <i class="fa fa-google-plus"></i>
+            </a>
+        </p>
+        <p>
+            Wanna buy the dev a cuppa coffee?
+        </p>
+        <p>
+            <a href="https://www.paypal.me/OliverDvorski" class="button" target="_blank">
+                <span class="icon is-small">
+                    <i class="fa fa-paypal"></i>
+                </span>
+                <span>PayPal.Me</span>
+            </a>
+        </p>
     </div>
 </footer>
